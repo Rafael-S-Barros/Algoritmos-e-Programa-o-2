164fecb5ac424302d05c2ab839523117ca8bb88a
Rafael da Silva Barros <rafaelbarros0511445@gmail.com>
2026-08-13 23:12:36 +0000

    Exercício 2

--- exercicio2.c
+++ exercicio2.c
@@ -5,6 +5,7 @@ int main(){
     printf("Ciência da Computação\n");
     printf("Algoritmos e Programação II\n");
     printf("Turma 01P\n");
+    printf("Ano de fundação do FCI: 1970\n");
 
     return 0;
 }
